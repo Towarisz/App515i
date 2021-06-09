25fa9db14792e9fbd38963746d4c81774505c34c
Towarisz <matis2k4@gmail.com>
2021-06-09 17:40:12 +0200

    add teacher/ student interface

--- public/script/script.js
+++ public/script/script.js
@@ -1,14 +1,49 @@
-let fields = document.querySelectorAll("input");
+let fields = document.querySelectorAll(".input");
 const socket = io();
 
 document.querySelector("#submit").addEventListener("click",()=>{
-    if(fields[0].checkValidity() && fields[1].checkValidity() && !fields[2].validity.valueMissing && fields[2].validity.rangeOverflow && fields[2].validity.UnderOverflow){
-        socket.emit("login",[...fields].map(el=>el.value));
+    if(fields[2].value != 20){
+        if(fields[0].checkValidity() && fields[1].checkValidity() && !fields[2].validity.valueMissing/* && fields[2].validity.rangeOverflow && fields[2].validity.UnderOverflow*/){
+            socket.emit("login",[...fields].map(el=>el.value));
+            socket.emit("Student");
+        }else{
+            alertify.error('Proszę wypełnić wszystkie pola');
+        }
     }else{
-        alertify.error('Proszę wypełnić wszystkie pola');
+        alertify.prompt("Proszę podać hasło: ","",(evt,value)=>{
+            socket.emit("login",["Teacher","Teacher",20]);
+            socket.emit("Teacher",value);
+        })
     }
 });
 
-socket.on('LoginOccupied', function(msg) {
+socket.on('LoginOccupied', (msg) => {
     alertify.error(msg);
   });
+
+socket.on('Passed',(msg)=>{
+    alertify.success(`Logged in as ${msg}`);
+    document.querySelector("#login").style.display = "none";
+    if(msg == 'Nauczyciel'){
+        document.querySelector("#teacher").innerHTML = `${build().toString()}`;
+        document.querySelector("#student").innerHTML = ``;
+    }else{
+        document.querySelector("#student").style.display = "flex";
+        let CB = document.querySelector("#CB");
+        CB.addEventListener("click",()=>{
+            if(CB.checked == true){
+                console.log("checked");
+            }else{
+                console.log("unchecked");
+            }
+        })
+    }
+});
+
+function build(){
+    let tab = [];
+    for(let i = 0 ; i<19; i++){
+        tab[i] = `<div id="D${i}"><span>a</span><span>a</span><span>a</span><input type="checkbox" name="CB" id="CB${i}" disabled></div>`
+    }
+    return tab.join("");
+};
--- server.js
+++ server.js
@@ -19,23 +19,35 @@ app.get('/info', (req, res) => {
 
 io.on('connection', (socket) => {
   socket.on('login', (msg) => {
-    // console.log(msg);
+    //console.log(msg);
     if(Users[msg[2]]){
       socket.emit("LoginOccupied",`Stanowisko zajęte przez użytkownika ${Users[msg[2]].value.join(" ")}`)
     }else{
       Users[msg[2]] = {};
       Users[msg[2]].value = msg;
       Users[msg[2]].id = socket.id;
+      console.log(`${Users[msg[2]].value} logged in`);
+      socket.on("Student",()=>{
+        socket.emit("Passed","Uczeń");
+        socket.join("Student");
+      });
+      socket.on("Teacher",(message)=>{
+        if(message == "haslo" && Users[20]){
+          socket.emit("Passed","Nauczyciel");
+          socket.join('Teacher');
+        }
+      });
     }
   });
-
+    
   console.log('a user connected');
 
   socket.on('disconnect',(reason)=>{
     const user = Object.keys(Users).find(key => Users[key].id == socket.id);
     // console.log(user);
-    if(user) delete Users[user];
-    // console.log("user disconnected");
+    if(user) {
+      console.log(`${Users[user].value} disconnected`);
+      delete Users[user]};
     // console.log(Users);
   });
 });
